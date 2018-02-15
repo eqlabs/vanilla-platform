b01ad2e8b090723350f4c3dcf22e0d9196437d1f
Jani Anttonen <jantto@utu.fi>
2018-02-15 10:00:37 +0200

    write comment to debuggable

--- contracts/Debuggable.sol
+++ contracts/Debuggable.sol
@@ -1,5 +1,11 @@
 pragma solidity ^0.4.18;
 
+/**
+ * @title Debuggable
+ * @dev Contract that's meant to be extended in other contracts to give them
+ * a debug event to test the code with. Just import, "SampleContract is Debuggable"
+ * and then in the SampleContract, writing debug("event") will write an event in the blockchain.
+ */
 contract Debuggable {
   event DebugEvent(string message);
   function debug(string message) public {
